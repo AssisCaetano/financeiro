bf6e3e010b25343153cda5fdeb7944a26a4a788d
AssisCaetano <deassiscaetano29@gmail.com>
2024-10-09 21:13:27 -0300

    Foi finalizado o CRUD de ContasAPagar, falta criar as validações.

--- src/main/java/com/controle/financeiro/domain/service/ContasAPagarService.java
+++ src/main/java/com/controle/financeiro/domain/service/ContasAPagarService.java
@@ -1,5 +1,10 @@
 package com.controle.financeiro.domain.service;
 
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,16 +19,57 @@ public class ContasAPagarService {
     @Autowired
     private ContasAPagarRepository contasAPagarRepository;
 
+    //cadastrar conta
     public ContasAPagar saveConta(ContasAPagarDto contasAPagarDto){
 
-        double juroEmcimaDoCapitalInicial = contasAPagarDto.capitalInicial() * contasAPagarDto.juros();
+        double juroEmcimaDoCapitalInicial = contasAPagarDto.capitalInicial() * contasAPagarDto.taxaDeJuros();
         double saldoAtualizado = contasAPagarDto.capitalInicial() + juroEmcimaDoCapitalInicial;
+
         ContasAPagar contas = new ContasAPagar();
         BeanUtils.copyProperties(contasAPagarDto, contas);
+
         contas.setValorDoJuros(juroEmcimaDoCapitalInicial);
         contas.setSaldoDevedor(saldoAtualizado);
 
         return contasAPagarRepository.save(contas);
       
     }
+
+      //atualiza uma conta
+      public Optional<ContasAPagar> atualizarContasAPagar(UUID id, ContasAPagarDto contasAPagarDto){
+
+        Optional<ContasAPagar> contaByID = contasAPagarRepository.findById(id);
+
+        double juroEmcimaDoCapitalInicial = contasAPagarDto.capitalInicial() * contasAPagarDto.taxaDeJuros();
+        double saldoAtualizado = contasAPagarDto.capitalInicial() + juroEmcimaDoCapitalInicial;
+
+        ContasAPagar contas = contaByID.get();
+        BeanUtils.copyProperties(contasAPagarDto, contas);
+
+        contas.setValorDoJuros(juroEmcimaDoCapitalInicial);
+        contas.setSaldoDevedor(saldoAtualizado);
+        contasAPagarRepository.save(contas);
+
+        return contasAPagarRepository.findById(id);
+    }
+
+    //retorna uma de lista de contas
+    public List<ContasAPagar> getContasAPagarAll(){
+        List<ContasAPagar> listaAPagar = contasAPagarRepository.findAll();
+        return listaAPagar;
+    }
+
+    //retorna uma unica conta
+    public Optional<ContasAPagar> getContasAPagarById(UUID id){
+            Optional<ContasAPagar> contaById = contasAPagarRepository.findById(id);
+            return contaById;
+    }
+    //deletando conta
+    public Optional<ContasAPagar> deletarConta(UUID id){
+        Optional<ContasAPagar> byId = contasAPagarRepository.findById(id);
+        contasAPagarRepository.deleteById(id);
+        return byId;
+       
+    }
+
 }
