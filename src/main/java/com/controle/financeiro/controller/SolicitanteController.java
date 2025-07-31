e7b46153dce5b5f362b2d7483517a21a7121265a
AssisCaetano <deassiscaetano29@gmail.com>
2025-07-30 21:30:17 -0300

    Foi colocado arquivos novos

--- src/main/java/com/controle/financeiro/controller/SolicitanteController.java
+++ src/main/java/com/controle/financeiro/controller/SolicitanteController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +17,6 @@ import com.controle.financeiro.domain.model.Solicitante;
 import com.controle.financeiro.domain.service.SolicitanteService;
 import com.controle.financeiro.dto.SolicitanteDto;
 
-@Controller
-@RequestMapping("/cadastrar")
 @Controller
 @RequestMapping("/cadastrar")
 public class SolicitanteController {
